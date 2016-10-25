c7b99d013094c96b2c0d30a396098d3923934f70
YuviPanda <yuvipanda@gmail.com>
2016-10-24 21:10:04 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jupyterhub-noteboard',
+    version='0.1',
+    description='THW Berkeley specific haaack',
+    url='https://github.com/yuvipanda/noteboard',
+    author='Yuvi Panda',
+    author_email='yuvipanda@riseup.net',
+    license='3 Clause BSD',
+    packages=find_packages()
+)
